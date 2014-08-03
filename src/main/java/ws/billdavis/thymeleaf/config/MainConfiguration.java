1d1689fbcad75f92748733cb98ff8d1cfcdde787
Bill Davis <bdavisx@yahoo.com>
2014-08-02 19:04:52 -0500

    Link working on the list.

--- src/main/java/ws/billdavis/thymeleaf/config/MainConfiguration.java
+++ src/main/java/ws/billdavis/thymeleaf/config/MainConfiguration.java
@@ -1,11 +1,18 @@
 package ws.billdavis.thymeleaf.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import ws.billdavis.thymeleaf.employee.EmployeeQueryService;
 
 @Configuration
 @Profile("default")
 public class MainConfiguration {
+
+    @Bean
+    public EmployeeQueryService employeeQueryService() {
+        return new EmployeeQueryService();
+    }
 
 }
 
--- src/main/java/ws/billdavis/thymeleaf/employee/EmployeeController.java
+++ src/main/java/ws/billdavis/thymeleaf/employee/EmployeeController.java
@@ -1,15 +1,23 @@
 package ws.billdavis.thymeleaf.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class EmployeeController {
+    @Autowired
+    private EmployeeQueryService employeeQueryService;
 
     @RequestMapping(value = "/employeeList", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		return "employeeList";
+	public String get( Model model ) {
+        final List<EmployeeListQTO> employeeList = employeeQueryService.getEmployeeList();
+        model.addAttribute( "employees", employeeList );
+        return "employeeList";
 	}
 }
